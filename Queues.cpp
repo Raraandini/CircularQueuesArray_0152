eac19e7cd2c7795c76fffe84355e8b694a7504e0
Raraandinii <raraandinib@gmail.com>
2025-06-12 16:42:46 +0700

    Jika antrian melingkar (FRONT > REAR), tampilkan elemen dari FRONT hingga akhir array, lalu lanjutkan dari awal array hingga REAR

--- Queues.cpp
+++ Queues.cpp
@@ -96,6 +96,24 @@ public:
             
             cout << endl;
         }
-        
+        else
+        {
+            // Jika FRONT_position > RERA_position, iterasi FRONT hingga akhir array
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
     }
 };
+
+
