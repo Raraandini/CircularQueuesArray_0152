451ebd4faa271b5fb556864d500bec726b5cecb4
Raraandinii <raraandinib@gmail.com>
2025-06-24 18:30:34 +0700

    Documentation program of Circular Queues Array

--- Queues.cpp
+++ Queues.cpp
@@ -1,23 +1,70 @@
+/**
+* @mainpage Documentation Circular Queues
+*
+* @section Introduction
+* Project ini merupakan project struktur data
+* menggunakan sruktur data queues dengan pendekatan circular arrays.
+* 
+* @section Operations
+* Project ini memiliki beberapa operasi antara lain:
+* 1. Insert
+* 2. Delete
+* 3. Display
+*
+* @section Cara Pengunaan
+* Berikut beberapa menu yang bisa digunakan:
+* 1. en queue
+* 2. de queue
+* 3. display
+* 4. exit
+*
+* @author Profil
+* - nama : Rara Andini Batrisyia
+* - nim : 20240140152
+* - kelas : C
+* @brief
+* @version 1.0
+* @date 2025-06-24
+* 
+* @copyright rara.andini.ft24@mail.umy.ac.id (c) 2025
+*/
+
+
 #include <iostream>
 using namespace std;
-
+/**
+ * @class Queues
+ * @brief calss ini untuk operasi lengkap queues
+ * 
+ * 
+ */
 class Queues 
 {
 private:
-    static const int max = 5;
-    int FRONT, REAR;
-    int queue_array[5];
+    int FRONT; ///< variabel private front untuk menyimpan posisi depan antrian
+    int REAR;  ///< variabel private rear untuk menyimpan posisi belakang antrian
+    int max = 5; ///< variabel private max untuk menyimpan ukuran maximum antrian
+    int queue_array[5]; ///< variabel private queue_array untuk menyimpan elemen antrian
 
 public:
+    /**
+     * @brief Construct a new Queues object
+     * set default queues null
+     * with front = -1 and rear = -1
+     */
     Queues()
     {
         FRONT = -1;
         REAR = -1;
     }
 
+    /**
+     * @brief method untuk memasukkan data dalam antrian
+     * data dimasukkan dalam variable queue_array 
+     */
     void insert()
     {
-        int num;
+        int num; ///< variabel num untuk menyimpan nilai
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
@@ -45,6 +92,10 @@ public:
         queue_array[REAR] = num;
     }
 
+    /**
+     * @brief method untuk menghapus data dalam antrian element 
+     * data dihapuskkan dari dalam variable queue_array element terakhir pada variable rear
+     */
     void remove()
     {
         // Cek apakah antrian kosong
@@ -71,10 +122,14 @@ public:
         }
     }  
 
+    /**
+     * @brief method untuk menampilkan data dalam antrian
+     * data ditampilkan yang berada dalam variable queue_array 
+     */
     void display()
     {
-        int FRONT_position = FRONT;
-        int REAR_position = REAR;
+        int FRONT_position = FRONT; ///< variable front_position untuk menandakan posisi element pertama pada variable front
+        int REAR_position = REAR; ///< variable rear_position untuk menandakan posisi element terakhir pada variable rear
 
         // Cek apakah antrian kosong
         if (FRONT == -1)
@@ -116,10 +171,15 @@ public:
     }
 };
 
+/**
+     * @brief method untuk menjalankan program
+     *
+     * @return int 
+     */
 int main()
 {
-    Queues q;
-    char ch;
+    Queues q; ///< objek untuk menggunakan member yang ada pada class queues
+    char ch; ///< variable ch untuk menyimpan pilihan pada menu yang diberikan
 
     while(true)
     {
@@ -169,3 +229,4 @@ int main()
         }
     }
 };
+ 
